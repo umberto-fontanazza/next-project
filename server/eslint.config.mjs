f264abb313d73c5b39ba864c417bb08832ee7ea2
Umberto Fontanazza <umberto.fontanazza@gmail.com>
2024-10-19 19:58:57 +0200

    Add workspace extensions

--- server/eslint.config.mjs
+++ server/eslint.config.mjs
@@ -7,5 +7,5 @@ import eslintConfigPrettier from "eslint-config-prettier";
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
-  eslintConfigPrettier,
+  eslintConfigPrettier
 );
